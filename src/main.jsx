847f1ee43e8f08188a2696cda05a48c807821076
nimra13045 <nimranazir447@gmail.com>
2026-08-29 17:34:48 +0500

    Fix GitHub Pages React Router

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+
 import App from "./App";
 import Productcontxt from "./context/productcontxt";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/product_managment">
       <Productcontxt>
         <App />
       </Productcontxt>
